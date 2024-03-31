def08f1eaaf67833e671b2464b66ee32f015ad47
ChingWhee <wongchingwhee@gmail.com>
2024-03-31 14:17:53 +0800

    alex code

---
+++ alex/colour_sensor.ino
@@ -0,0 +1,87 @@
+#define S1 44
+#define S0 48
+#define S2 52
+#define S3 46
+#define sensorOut 50
+
+#define color_sensor_delay 50 //milliseconds
+
+unsigned long red_freq;
+unsigned long green_freq;
+unsigned long blue_freq;
+
+void setup_colour_sensor() {
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(sensorOut, INPUT);
+  
+  // Setting frequency scaling to 20%
+  digitalWrite(S0, HIGH);      
+  digitalWrite(S1, LOW);
+}
+
+// Take 5 readings and return average frequency
+int average_freq() {
+  int reading;
+  int total = 0;
+
+  for (int i = 0; i < 5; i+=1) {
+    reading = pulseIn(sensorOut, LOW);
+    total += reading;
+    delay(20);
+  }
+  
+  return total / 5;
+}
+
+// Find the color of the paper
+void findColour() { 
+  // Setting RED (R) filtered photodiodes to be read
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, LOW);
+  delay(color_sensor_delay);
+
+  // Reading the output frequency for RED
+  red_freq = average_freq();
+  delay(color_sensor_delay);
+
+  // Setting GREEN (G) filtered photodiodes to be read
+  digitalWrite(S2, HIGH);
+  digitalWrite(S3, HIGH);
+  delay(color_sensor_delay);
+
+  // Reading the output frequency for GREEN
+  green_freq = average_freq();
+  delay(color_sensor_delay);
+
+  // Setting BLUE (B) filtered photodiodes to be read
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, HIGH);
+  delay(color_sensor_delay);
+
+  // Reading the output frequency for BLUE
+  blue_freq = average_freq();
+  delay(color_sensor_delay);
+  Serial.print("red : ");
+  Serial.println(red_freq);
+  Serial.print("green : ");
+  Serial.println(green_freq);
+  Serial.print("blue : ");
+  Serial.println(blue_freq);
+
+}
+
+void sendColour(uint32_t dist) {
+  TPacket colorPacket;
+  colorPacket.packetType = PACKET_TYPE_RESPONSE;
+  colorPacket.command = RESP_COLOUR;
+  
+  colorPacket.params[0] = red_freq;
+  colorPacket.params[1] = green_freq;
+  colorPacket.params[2] = blue_freq;
+  colorPacket.params[3] = dist;
+  
+  sendResponse(&colorPacket);  
+}
---
+++ alex/robotlib.ino
@@ -0,0 +1,126 @@
+#include <AFMotor.h>
+
+// Motor control to be adjusted
+#define FRONT_LEFT   4 // M4 on the driver shield
+#define FRONT_RIGHT  3 // M1 on the driver shield
+#define BACK_LEFT    1 // M3 on the driver shield
+#define BACK_RIGHT   2 // M2 on the driver shield
+
+AF_DCMotor motorFL(FRONT_LEFT);
+AF_DCMotor motorFR(FRONT_RIGHT);
+AF_DCMotor motorBL(BACK_LEFT);
+AF_DCMotor motorBR(BACK_RIGHT);
+
+void move(float speed, int direction)
+{
+  int speed_scaled = (speed/100.0) * 255;
+  motorFL.setSpeed(speed_scaled);
+  motorFR.setSpeed(speed_scaled);
+  motorBL.setSpeed(speed_scaled);
+  motorBR.setSpeed(speed_scaled);
+
+  switch(direction)
+    {
+      case BACK:
+        motorFL.run(BACKWARD);
+        motorFR.run(BACKWARD);
+        motorBL.run(FORWARD);
+        motorBR.run(FORWARD); 
+    break;
+      case GO:
+        motorFL.run(FORWARD);
+        motorFR.run(FORWARD);
+        motorBL.run(BACKWARD);
+        motorBR.run(BACKWARD); 
+    break;
+      case CCW:
+        motorFL.run(BACKWARD);
+        motorFR.run(FORWARD);
+        motorBL.run(FORWARD);
+        motorBR.run(BACKWARD); 
+    break;
+      case CW:
+        motorFL.run(FORWARD);
+        motorFR.run(BACKWARD);
+        motorBL.run(BACKWARD);
+        motorBR.run(FORWARD); 
+    break;
+      case STOP:     
+        motorFL.run(STOP);
+        motorFR.run(STOP);
+        motorBL.run(STOP);
+        motorBR.run(STOP); 
+    break;
+    }
+}
+
+void forward(float dist, float speed)
+{
+  if(dist > 0){
+    deltaDist = dist;
+  }else{
+    deltaDist=9999999;
+  }
+  newDist=forwardDist + deltaDist;
+  dir = (TDirection) FORWARD;
+  move(speed, FORWARD);
+}
+
+void backward(float dist, float speed)
+{
+  if(dist > 0){
+    deltaDist = dist;
+  }else{
+    deltaDist=9999999;
+  }
+  newDist= reverseDist + deltaDist;
+  dir = (TDirection) BACKWARD;
+  move(speed, BACKWARD);
+}
+
+unsigned long computeDeltaTicks(float ang){
+  unsigned long ticks = (unsigned long)((ang * alexCirc * COUNTS_PER_REV) / (360.0 * WHEEL_CIRC));
+  if(ticks < 1){
+    ticks = 1;
+  }
+  return ticks;
+}
+
+void ccw(float dist, float speed)//left
+{
+  dir = (TDirection) CCW;
+  move(speed, CCW);
+}
+
+void left(float ang , float speed){
+  if(ang == 0){
+    deltaTicks=99999999;
+  }else{
+    deltaTicks=computeDeltaTicks(ang);
+    targetTicks = leftReverseTurns + deltaTicks;
+  }
+  ccw(ang,speed);
+}
+
+void cw(float dist, float speed)//right
+{
+  dir = (TDirection) CW;
+  move(speed, CW);
+}
+
+void right(float ang , float speed){
+  if(ang == 0){
+    deltaTicks=99999999;
+  }else{
+    deltaTicks=computeDeltaTicks(ang);
+    targetTicks = rightReverseTurns + deltaTicks;
+  }
+  cw(ang,speed);
+}
+
+void stop()
+{
+  dir = (TDirection) STOP;
+  move(0, STOP);
+}
+
