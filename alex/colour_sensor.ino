0afb7f4fbae87c82a784bd96fc56a4e1d94941bf
rexkoh425 <141440631+rexkoh425@users.noreply.github.com>
2024-04-11 19:35:05 +0800

    Update colour_sensor.ino

--- alex/colour_sensor.ino
+++ alex/colour_sensor.ino
@@ -1,14 +1,18 @@
-#define S1 44
-#define S0 48
-#define S2 52
-#define S3 46
-#define sensorOut 50
+#define S1 44 //brown//TO CHANGE
+#define S0 48 //orange
+#define S2 52 //blue
+#define S3 46 //green
+#define sensorOut 50 //white
 
 #define color_sensor_delay 50 //milliseconds
+#define ShroudMouthToUltrasonic 10 //cm//to change
+#define DistancetoStop 2 //cm//dist to stop from object
 
 unsigned long red_freq;
 unsigned long green_freq;
 unsigned long blue_freq;
+
+unsigned long linearactuatordist = 0;
 
 void setup_colour_sensor() {
   pinMode(S0, OUTPUT);
@@ -67,6 +71,11 @@ void findColor() {
 }
 
 void sendColor() {
+  uint32_t currentdist = getUltrasonicDistance() - ShroudMouthToUltrasonic;
+  if(currentdist< DistancetoStop){
+    distance_forward(currentdist -DistancetoStop );
+    linearactuatordist = currentdist - DistancetoStop;
+  }
   TPacket colorPacket;
   colorPacket.packetType = PACKET_TYPE_RESPONSE;
   colorPacket.command = RESP_COLOR;
@@ -76,4 +85,5 @@ void sendColor() {
   colorPacket.params[2] = blue_freq;
   
   sendResponse(&colorPacket);  
+  distance_reverse(linearactuatordist + 1);
 }
