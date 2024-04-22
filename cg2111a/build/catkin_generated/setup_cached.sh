c206e7c99c46c3caa2d74c198beb5930c31fc6fc
Roma <romajoshi637@gmail.com>
2024-04-22 20:36:28 +0800

    final

---
+++ cg2111a/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PWD='/home/RRRSC/CG2111A-B04-5/ROS/build'
+export ROS_PACKAGE_PATH='/home/RRRSC/cg2111a/src:/home/RRRSC/CG2111A-B04-5/ROS/src:/home/RRRSC/cg2111a/src:/opt/ros/noetic/share'
