e6c3b48891e8e48847e6bd9cf899a0c744d082ea
rexkoh425 <141440631+rexkoh425@users.noreply.github.com>
2024-04-25 21:53:36 +0800

    Update colour.cpp

--- RPi/colour.cpp
+++ RPi/colour.cpp
@@ -2,7 +2,7 @@
 #include "prints.h"
 #define SHROUDTOULTRA 7
 
-float getPercentDiff(uint32_t a, uint32_t b) {
+float getPercentDiff(uint32_t a, uint32_t b) { // returns absolute difference between the two colours
 	uint32_t diff = (a > b) ? a-b : b-a; // abs diff
 	uint32_t large = (a > b) ? a : b; // max
 	return (float) diff / large * 100.0;
@@ -16,16 +16,16 @@ void handleColor(TPacket *packet) {
 	float redBlueDiff = getPercentDiff(red, blue);
 	float blueGreenDiff = getPercentDiff(blue, green);
 	if(keyboardMode == 1 || keyboardMode == 2){
-		print_colourf(red , green , blue , redGreenDiff , redBlueDiff, blueGreenDiff);	
+		print_colourf(red , green , blue , redGreenDiff , redBlueDiff, blueGreenDiff);	//for mode 1 , 2
 	}else{
-		print_colourw(red , green , blue , redGreenDiff , redBlueDiff, blueGreenDiff);	
+		print_colourw(red , green , blue , redGreenDiff , redBlueDiff, blueGreenDiff);	//mode 3 , 4
 	}
 }
 
 void handleDistance(TPacket *packet) {
 	uint32_t distance = packet->params[0];
-	printf("Ultrasonic Distance:\t\t%d cm\n", distance);
-	printf("Actual distance from shroud is \t\t%d cm\n" , distance - SHROUDTOULTRA);
+	printf("Ultrasonic Distance:\t\t%d cm\n", distance); // returns distance from ultrasonic to object
+	printf("Actual distance from shroud is \t\t%d cm\n" , distance - SHROUDTOULTRA); // return distance of shroud from object
 	const int DIST_THRESHOLD = 25;
 	if (distance < DIST_THRESHOLD) printf("WALL NEARBY! SLOW DOWN!\n");
 }
